8e4cbb01f710536cd3dda2498683f464841ffc8a
SavenkoRodion <savenko662@gmail.com>
2025-11-15 14:56:31 +0100

    dummy layout

--- ui/quiz-generator/src/main.tsx
+++ ui/quiz-generator/src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import App from "./App.tsx";
+import Layout from "./components/Layout.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/create-quiz" element={<>test</>} />
-        <Route path="/solve-quiz" element={<>test1</>} />
+        <Route element={<Layout />}>
+          <Route index path="/" element={<>test</>} />
+
+          <Route path="/create-quiz" element={<>test</>} />
+          <Route path="/solve-quiz" element={<>test1</>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
