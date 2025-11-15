21b980a9eb0c33fd040435c9c27e56ff9eb40ee4
SavenkoRodion <75912132+SavenkoRodion@users.noreply.github.com>
2025-11-15 16:06:07 +0100

    Initial routing implementation (#1)
    * Initial routing
    
    * dummy layout

--- ui/quiz-generator/src/main.tsx
+++ ui/quiz-generator/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="/" element={<>test</>} />
+
+          <Route path="/create-quiz" element={<>test</>} />
+          <Route path="/solve-quiz" element={<>test1</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
