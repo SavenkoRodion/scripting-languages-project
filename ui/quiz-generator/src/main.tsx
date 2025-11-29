41afe1414c1668187a3901fa6f717d5e9ebeddb8
paula048 <97178187+paula048@users.noreply.github.com>
2025-11-29 15:55:07 +0100

    home page

--- ui/quiz-generator/src/main.tsx
+++ ui/quiz-generator/src/main.tsx
@@ -7,6 +7,7 @@ import App from "./App.tsx";
 import "./index.css";
 import CreateQuizPage from "./pages/CreateQuizPage.tsx";
 import QuizListPage from "./pages/QuizListPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
 
 export const queryClient = new QueryClient();
 
@@ -20,18 +21,9 @@ createRoot(document.getElementById("root")!).render(
             {/* HomePage → "/" */}
             <Route
               index
-              element={
-                <div className="space-y-4">
-                  <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                    Home
-                  </h1>
-                  <p className="text-gray-700">
-                    Welcome to your quiz app. Use the navigation to create new
-                    quizzes or browse existing ones.
-                  </p>
-                </div>
-              }
+              element={<HomePage />}
             />
+
 
             {/* AddQuiz → "/create-quiz" (already implemented) */}
             <Route path="create-quiz" element={<CreateQuizPage />} />
---
+++ ui/quiz-generator/src/pages/HomePage.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router";
+import { useQuizList } from "../util/hooks";
+import '../fonts/PopArt-Regular.ttf'
+import '../styles/homeStyle.css'
+
+export default function HomePage() {
+  const { data: quizList } = useQuizList();
+
+  return (
+    <div style={{display: "flex", justifyContent: "center", textAlign: "center"}}>
+        <div>
+            <h1 className="text-indigo-700" style={{fontFamily: "PopArt", fontSize: "60px"}}>Welcome at Quiz Generator</h1>
+            <h2 style={{fontFamily: 'Montserrat, sans-serif', fontSize: "30px"}}>Easy, fast, and fun quiz creation.</h2>
+            <button
+            style={{backgroundColor: "#ff5722", marginTop: "30px"}}
+            className="
+            px-6 py-3
+            bg-indigo-600 
+            text-white 
+            font-semibold 
+            rounded-2xl
+            shadow-md 
+            transition-all 
+            hover:bg-indigo-700 
+            hover:shadow-lg 
+            active:scale-95">
+            Let's started
+            </button>
+        </div>
+
+
+        
+        
+
+    </div>
+  );
+}
